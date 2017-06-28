fb5aa3df148702039247c3f0cd57e58052751fe2
ustccjw <317713370@qq.com>
2017-06-28 18:03:05 +0800

    default flex none

--- test/index.js
+++ test/index.js
@@ -5,18 +5,18 @@ describe('box-flex', () => {
   test('box-flex: 0', async () => {
     const input = 'div { display: -webkit-box } span { -webkit-box-flex: 0; }'
     const res = await postcss([plugin()]).process(input)
-    expect(res.css).toBe('div > * { width: 1px; display: block; -webkit-box-flex: 1; -webkit-flex: 1; flex: auto } div { display: -webkit-box } span { -webkit-box-flex: 0; }')
+    expect(res.css).toBe('div > * { display: block; -webkit-box-flex: 0; -webkit-flex: 0; flex: none } div { display: -webkit-box } span { -webkit-box-flex: 0; }')
   })
 
   test('box-flex: 1', async () => {
     const input = 'div { display: -webkit-box } span { -webkit-box-flex: 1; }'
     const res = await postcss([plugin()]).process(input)
-    expect(res.css).toBe('div > * { width: 1px; display: block; -webkit-box-flex: 1; -webkit-flex: 1; flex: auto } div { display: -webkit-box } span { width: 1px; -webkit-box-flex: 1; }')
+    expect(res.css).toBe('div > * { display: block; -webkit-box-flex: 0; -webkit-flex: 0; flex: none } div { display: -webkit-box } span { width: 1px; -webkit-box-flex: 1; }')
   })
 
   test('width: auto; box-flex: 1', async () => {
     const input = 'div { display: -webkit-box } span { -webkit-box-flex: 1; width: auto; }'
     const res = await postcss([plugin()]).process(input)
-    expect(res.css).toBe('div > * { width: 1px; display: block; -webkit-box-flex: 1; -webkit-flex: 1; flex: auto } div { display: -webkit-box } span { -webkit-box-flex: 1; width: auto; }')
+    expect(res.css).toBe('div > * { display: block; -webkit-box-flex: 0; -webkit-flex: 0; flex: none } div { display: -webkit-box } span { -webkit-box-flex: 1; width: auto; }')
   })
 })
