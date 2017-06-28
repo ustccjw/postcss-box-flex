6425935aed06b97c4d91d29c921f414ebccedff4
ustccjw <317713370@qq.com>
2017-06-28 17:40:29 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import postcss from 'postcss'
 
 const plugin = postcss.plugin('postcss-box-flex', () => css => {
+  css.walkDecls(/^display$/, decl => {
+    if (/box$/.test(decl.value)) {
+      const rule = postcss.rule({ selector: `${decl.parent.selector} > *` })
+      rule.append(postcss.decl({ prop: 'display', value: 'block' }))
+      rule.append(postcss.decl({ prop: '-webkit-box-flex', value: '1' }))
+      rule.append(postcss.decl({ prop: '-webkit-flex', value: '1' }))
+      rule.append(postcss.decl({ prop: 'flex', value: 'auto' }))
+      css.prepend(rule)
+    }
+  })
+
   css.walkDecls(/box-flex$/, decl => {
     if (+decl.value === 1) {
       let width = false
@@ -14,17 +25,6 @@ const plugin = postcss.plugin('postcss-box-flex', () => css => {
         const widthDecl = postcss.decl({ prop: 'width', value: '1px' })
         decl.parent.prepend(widthDecl)
       }
-    }
-  })
-
-  css.walkDecls(/^display$/, decl => {
-    if (/box$/.test(decl.value)) {
-      const rule = postcss.rule({ selector: `${decl.parent.selector} > *` })
-      rule.append(postcss.decl({ prop: 'display', value: 'block' }))
-      rule.append(postcss.decl({ prop: '-webkit-box-flex', value: '1' }))
-      rule.append(postcss.decl({ prop: '-webkit-flex', value: '1' }))
-      rule.append(postcss.decl({ prop: 'flex', value: 'auto' }))
-      css.prepend(rule)
     }
   })
 })
