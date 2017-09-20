1f661788b9328c002a60c6123fcf37984c2953a0
ustccjw <317713370@qq.com>
2017-09-20 18:44:55 +0800

    fix: remove before/after selector

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import postcss from 'postcss'
 
 const plugin = postcss.plugin('postcss-box-flex', () => css => {
   css.walkDecls(/^display$/, decl => {
-    if (/box$/.test(decl.value)) {
+    if (/box$/.test(decl.value) && !/(:before|:after)$/.test(decl.parent.selector)) {
       const rule = postcss.rule({ selector: `${decl.parent.selector} > *, ${decl.parent.selector}::before, ${decl.parent.selector}::after` })
       rule.append(postcss.decl({ prop: '-webkit-box-flex', value: '0' }))
       rule.append(postcss.decl({ prop: '-webkit-flex', value: 'none' }))
