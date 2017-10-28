3195f84c4f79fbf1af522a6275c97c3bf8d55477
ustccjw <317713370@qq.com>
2017-10-28 10:44:07 +0800

    format

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import postcss from 'postcss'
 
-const plugin = postcss.plugin('postcss-box-flex', () => css => {
-  css.walkDecls(/^display$/, decl => {
-    if (/box$/.test(decl.value) && !/(:before|:after)$/.test(decl.parent.selector)) {
-      const rule = postcss.rule({ selector: `${decl.parent.selector} > *, ${decl.parent.selector}::before, ${decl.parent.selector}::after` })
+const plugin = postcss.plugin('postcss-box-flex', () => root => {
+  root.walkDecls(/^display$/, decl => {
+    const { parent, value } = decl
+    const { selector } = parent
+    if (/box$/.test(value) && !/(:before|:after)$/.test(selector)) {
+      const rule = postcss.rule({ selector: `${selector} > *, ${selector}::before, ${selector}::after` })
       rule.append(postcss.decl({ prop: '-webkit-box-flex', value: '0' }))
       rule.append(postcss.decl({ prop: '-webkit-flex', value: 'none' }))
       rule.append(postcss.decl({ prop: 'flex', value: 'none' }))
-      css.prepend(rule)
+      root.prepend(rule)
     }
   })
 })
