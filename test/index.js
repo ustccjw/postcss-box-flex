dbe4309e57a692841a21667d53c7c5efca4955c6
ustccjw <317713370@qq.com>
2017-10-28 13:10:51 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,28 @@
 import postcss from 'postcss'
 import plugin from '../src'
 
+function format(str) {
+  return str.replace(/\s+/g, ' ').trim()
+}
+
 describe('box-flex', () => {
   test('box-flex', async () => {
-    const input = 'div { display: -webkit-box }'
+    const input = `
+      div {
+        display: -webkit-box;
+      }
+    `
     const res = await postcss([plugin()]).process(input)
-    expect(res.css).toBe('div > *, div::before, div::after { -webkit-box-flex: 0; -webkit-flex: none; flex: none }\ndiv { display: -webkit-box }')
+    const str = `
+      div > *, div::before, div::after {
+        -webkit-box-flex: 0;
+        -webkit-flex: none;
+        flex: none;
+      }
+      div {
+        display: -webkit-box;
+      }
+    `
+    expect(format(res.css)).toBe(format(str))
   })
 })
