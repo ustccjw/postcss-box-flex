3c5987ff1e87d4fa2faffb3b40fe1be638bb6f24
ustccjw <317713370@qq.com>
2017-11-02 21:35:00 +0800

    fix: more selector

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const plugin = postcss.plugin('postcss-box-flex', () => root => {
   root.walkDecls(/^display$/, decl => {
     const { parent, value } = decl
     const { selector } = parent
-    if (/box$/.test(value) && !/(:before|:after)$/.test(selector)) {
-      const rule = postcss.rule({ selector: `${selector} > *, ${selector}::before, ${selector}::after` })
-      rule.append(postcss.decl({ prop: '-webkit-box-flex', value: '0' }))
-      rule.append(postcss.decl({ prop: '-webkit-flex', value: 'none' }))
-      rule.append(postcss.decl({ prop: 'flex', value: 'none' }))
-      root.prepend(rule)
+    if (/box$/.test(value)) {
+      selector.split(',').forEach(selector2 => {
+        if (!/(:before|:after)$/.test(selector)) {
+          const rule = postcss.rule({ selector: `${selector2} > *, ${selector2}::before, ${selector2}::after` })
+          rule.append(postcss.decl({ prop: '-webkit-box-flex', value: '0' }))
+          rule.append(postcss.decl({ prop: '-webkit-flex', value: 'none' }))
+          rule.append(postcss.decl({ prop: 'flex', value: 'none' }))
+          root.prepend(rule)
+        }
+      })
     }
   })
 })
